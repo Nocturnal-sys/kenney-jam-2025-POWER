536b62e58974ea40028fc36bab8ff3c895cf12ec
Nocturnal-sys <214055938+Nocturnal-sys@users.noreply.github.com>
2025-07-22 13:08:37 +0100

    add mute button to volume slider

--- Scripts/volume_slider.gd
+++ Scripts/volume_slider.gd
@@ -1,6 +1,7 @@
 extends HSlider
 
-@onready var label: Label = $Label
+@onready var label: Label = $Value
+@onready var mute: CheckButton = $Mute
 
 @export
 var bus_name: String
@@ -13,8 +14,12 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	label.text = str(int(value*10000))
+	label.text = str(int(value*100))
 
 
 func _on_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
+
+
+func _on_mute_toggled(toggled_on: bool) -> void:
+	AudioServer.set_bus_mute(bus_index, not AudioServer.is_bus_mute(bus_index))
